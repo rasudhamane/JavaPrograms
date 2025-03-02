4684d587e44d0257cc5e5d5b1013177da68293ce
rasudhamane <rasudhamane69@gmail.com>
2025-03-02 16:22:19 +0530

    my 2nd commit

---
+++ addition2.java
@@ -0,0 +1,16 @@
+package package1;
+
+public class addition2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		addtion.add1();
+		addtion.add2();
+		
+		addtion ref =new addtion();
+	     ref.add4();
+
+	}
+
+}
---
+++ addtion.java
@@ -0,0 +1,31 @@
+package package1;
+
+public class addtion {
+	public static void add1() {
+
+	}
+
+	static void add2() {
+
+	}
+
+	private static void add3() {
+
+	}
+
+	protected void add4() {
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		add1();
+		add2();
+		add3();
+		
+		addtion ref=new addtion();
+		ref.add4();
+
+	}
+
+}
